1ed81361aceac85a2dc8c7f392269816c2e34e3a
Frida A <dafrireece@gmail.com>
2023-02-17 23:14:31 +0000

    Added projects

--- app.py
+++ app.py
@@ -2,10 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+PROJECTS = [
+      {"id" :1,
+      "title" : "Python",
+      "desc" :" EDA"
+      },
+  {"id" :1,
+      "title" : "Excel",
+      "desc" :" Dashboard"
+      },
+  {"id" :1,
+      "title" : "Tableau",
+      "desc" :" Dashboard"
+      },
+  {"id" :1,
+      "title" : "SQL",
+      "desc" :"Data Exploration"
+      }
+
+]
+
 
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html",projects= PROJECTS)
 
 
 if __name__ == "__main__":
